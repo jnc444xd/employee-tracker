aba86809b04cdccb8d437137da3dfcf031d473aa
[j chun] <[justinchun224@gmail.com]>
2023-10-12 20:50:11 -0700

    creating functions index.js

--- index.js
+++ index.js
@@ -10,9 +10,94 @@ const db = mysql.createConnection(
     console.log(`Connected to the company_db.`)
 );
 
-const viewDept = () => {
+const viewDepts = () => {
     const sql = `SELECT * FROM departments`
     // How to choose correct error message?
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.log(results);
+    });
+};
+
+const viewRoles = () => {
+    const sql = `SELECT * FROM roles`
+    
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.log(results);
+    });
+};
+
+const viewEmployees = () => {
+    const sql = `SELECT * FROM employees`
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.log(results);
+    });
+};
+
+const addDept = () => {
+    const sql = `
+    INSERT INTO 
+    VALUES
+    `
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.log(results);
+    });
+};
+
+const addRole = () => {
+    const sql = `
+    INSERT INTO 
+    VALUES
+    `
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.log(results);
+    });
+};
+
+const addEmployee = () => {
+    const sql = `
+    INSERT INTO 
+    VALUES
+    `
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.log(results);
+    });
+};
+
+const updateEmployee = () => {
+    const sql = `
+    UPDATE
+    SET
+    WHERE
+    `
+
     db.query(sql, (err, results) => {
         if (err) {
             res.status(500).json({ error: err.message });
