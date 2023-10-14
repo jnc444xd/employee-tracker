30ac4dbe3df180d1563e303d1d5652846ba8299a
[j chun] <[justinchun224@gmail.com]>
2023-10-14 15:55:11 -0700

    joined tables to display more info when viewing roles and employees

--- sql.js
+++ sql.js
@@ -41,10 +41,11 @@ const viewRoles = () => {
 
 const viewEmployees = () => {
     const sql = `
-    SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department_name, r.salary 
+    SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department_name, r.salary, CONCAT(m.first_name, " ", m.last_name) AS manager_name
     FROM employees e
     JOIN roles r ON e.role_id = r.id
     JOIN departments d ON r.department_id = d.id
+    JOIN employees m ON e.manager_id = m.id;
     `
 
     db.query(sql, (err, results) => {
