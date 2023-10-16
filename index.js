d8bacba26bb902032afe0f75b87a18b0aa2ebabb
[j chun] <[justinchun224@gmail.com]>
2023-10-15 20:13:19 -0700

    added banner to console.log inside init function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { starterPrompt } = require('./lib/cli');
 
-const init = () => starterPrompt();
+const init = () => {
+    console.log(`
+    +---+---+---+---+---+---+---+---+
+    | E | m | p | l | o | y | e | e |
+    +---+---+---+---+---+---+---+---+
+    | T | r | a | c | k | e | r |
+    +---+---+---+---+---+---+---+
+
+    `);
+    starterPrompt();
+};
 
 init();
